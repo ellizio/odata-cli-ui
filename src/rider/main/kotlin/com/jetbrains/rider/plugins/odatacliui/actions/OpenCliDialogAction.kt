f828adb2942c2c51ba349f18beaccb480469be52
Ellizio <profillive@mail.ru>
2024-03-21 18:08:58 +0300

    refactoring

--- src/rider/main/kotlin/com/jetbrains/rider/plugins/odatacliui/actions/OpenCliDialogAction.kt
+++ src/rider/main/kotlin/com/jetbrains/rider/plugins/odatacliui/actions/OpenCliDialogAction.kt
@@ -1,22 +1,17 @@
 package com.jetbrains.rider.plugins.odatacliui.actions
 
-import com.intellij.execution.filters.TextConsoleBuilderFactory
+import com.intellij.execution.ui.ConsoleView
 import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.rd.util.launchOnUi
-import com.intellij.openapi.rd.util.lifetime
-import com.intellij.openapi.rd.util.withBackgroundContext
-import com.intellij.openapi.rd.util.withUiContext
+import com.intellij.openapi.rd.util.*
 import com.intellij.openapi.vfs.VirtualFileManager
-import com.intellij.openapi.wm.ToolWindowAnchor
-import com.intellij.openapi.wm.ToolWindowManager
-import com.jetbrains.rider.plugins.odatacliui.Constants
 import com.jetbrains.rider.plugins.odatacliui.dialogs.CliDialog
 import com.jetbrains.rider.plugins.odatacliui.extensions.entityForAction
 import com.jetbrains.rider.plugins.odatacliui.models.CliDialogModel
 import com.jetbrains.rider.plugins.odatacliui.terminal.BatchCommandLineExecutor
+import com.jetbrains.rider.plugins.odatacliui.toolwindows.CliToolWindowManager
 import com.jetbrains.rider.projectView.workspace.isProject
 import com.jetbrains.rider.projectView.workspace.isWebReferenceFolder
 
@@ -44,20 +39,12 @@ class OpenCliDialogAction : AnAction() {
 
     private suspend fun executeCommand(project: Project, model: CliDialogModel)
     {
-        val consoleView = TextConsoleBuilderFactory.getInstance().createBuilder(project).console
+        var consoleView: ConsoleView? = null
         withUiContext {
-            val toolWindow = ToolWindowManager.getInstance(project).registerToolWindow(Constants.PLUGIN_NAME) {
-                anchor = ToolWindowAnchor.BOTTOM
-                canCloseContent = true
-            }
-            val content = toolWindow.contentManager.factory.createContent(consoleView.component, "Generate", true)
-            toolWindow.contentManager.addContent(content)
-            toolWindow.activate {
-                toolWindow.contentManager.setSelectedContent(content)
-            }
+            consoleView = CliToolWindowManager.getInstance(project).instantiateConsole()
         }
 
-        val executor = BatchCommandLineExecutor(project, model.buildCommand(), consoleView)
+        val executor = BatchCommandLineExecutor(project, model.buildCommand(), consoleView!!)
         executor.execute()
 
         VirtualFileManager.getInstance().refreshWithoutFileWatcher(true)
---
+++ src/rider/main/kotlin/com/jetbrains/rider/plugins/odatacliui/toolwindows/CliToolWindowManager.kt
@@ -0,0 +1,35 @@
+package com.jetbrains.rider.plugins.odatacliui.toolwindows
+
+import com.intellij.execution.filters.TextConsoleBuilderFactory
+import com.intellij.execution.ui.ConsoleView
+import com.intellij.openapi.components.Service
+import com.intellij.openapi.components.service
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.wm.ToolWindowAnchor
+import com.intellij.openapi.wm.ToolWindowManager
+import com.jetbrains.rider.plugins.odatacliui.Constants
+
+@Service(Service.Level.PROJECT)
+class CliToolWindowManager(private val project: Project) {
+    companion object {
+        fun getInstance(project: Project) = project.service<CliToolWindowManager>()
+    }
+
+    private val toolWindow by lazy {
+        ToolWindowManager.getInstance(project).registerToolWindow(Constants.PLUGIN_NAME) {
+            anchor = ToolWindowAnchor.BOTTOM
+            canCloseContent = true
+            // TODO: icon
+        }
+    }
+
+    fun instantiateConsole(): ConsoleView {
+        val consoleView = TextConsoleBuilderFactory.getInstance().createBuilder(project).console
+        val content = toolWindow.contentManager.factory.createContent(consoleView.component, "Generate", true)
+        toolWindow.contentManager.addContent(content)
+        toolWindow.activate {
+            toolWindow.contentManager.setSelectedContent(content)
+        }
+        return consoleView
+    }
+}
