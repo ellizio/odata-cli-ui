99d3ac9531755dd3cfc6ffce97fb2084d250caf3
ellizio <profillive@mail.ru>
2024-03-24 14:38:38 +0300

    clear code

--- src/rider/main/kotlin/com/jetbrains/rider/plugins/odatacliui/terminal/BatchCommandLine.kt
+++ src/rider/main/kotlin/com/jetbrains/rider/plugins/odatacliui/terminal/BatchCommandLine.kt
@@ -6,13 +6,4 @@ class BatchCommandLine {
     val commands: ArrayList<GeneralCommandLine> = arrayListOf()
 
     fun add(command: GeneralCommandLine) = commands.add(command)
-
-    fun getCommandLineString(): String {
-        val sb = StringBuilder()
-        for (i in commands.indices) {
-            sb.appendLine("[$i]: ${commands[i].commandLineString}")
-        }
-
-        return sb.toString()
-    }
 }
