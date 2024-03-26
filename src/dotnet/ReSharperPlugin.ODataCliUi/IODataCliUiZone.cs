6d486d28d3c616ba17f860bf50e1d878fa8d801e
ellizio <profillive@mail.ru>
2024-03-26 10:47:13 +0300

    prepare to release

--- src/dotnet/ReSharperPlugin.ODataCliUi/IODataCliUiZone.cs
+++ src/dotnet/ReSharperPlugin.ODataCliUi/IODataCliUiZone.cs
@@ -1,13 +1,12 @@
 using JetBrains.Application.BuildScript.Application.Zones;
-using JetBrains.ReSharper.Feature.Services.Daemon;
-using JetBrains.ReSharper.Psi;
-using JetBrains.ReSharper.Psi.CSharp;
+using JetBrains.ProjectModel;
+using JetBrains.ProjectModel.NuGet;
 
 namespace ReSharperPlugin.ODataCliUi
 {
     [ZoneDefinition]
     // [ZoneDefinitionConfigurableFeature("Title", "Description", IsInProductSection: false)]
-    public interface IODataCliUiZone : IZone
-    {
-    }
+    public interface IODataCliUiZone : IZone,
+        IRequire<IProjectModelZone>,
+        IRequire<INuGetZone>;
 }
