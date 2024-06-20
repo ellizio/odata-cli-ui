fa3a5cece582314514be227437042cc4c7ee8ac7
ellizio <profillive@mail.ru>
2024-06-20 11:25:46 +0300

    cli 0.3.1 support

--- src/rider/main/kotlin/ru/ellizio/odatacliui/extensions/ProtocolModelEx.kt
+++ src/rider/main/kotlin/ru/ellizio/odatacliui/extensions/ProtocolModelEx.kt
@@ -3,3 +3,16 @@ package ru.ellizio.odatacliui.extensions
 import com.jetbrains.rd.ide.model.DotnetToolVersionDefinition
 
 fun DotnetToolVersionDefinition.humanize() : String = "$major.$minor.$patch"
+
+fun DotnetToolVersionDefinition.greaterOrEquals(major: Int, minor: Int, patch: Int) : Boolean {
+    if (this.major > major)
+        return true
+
+    if (this.major == major && this.minor > minor)
+        return true
+
+    if (this.major == major && this.minor == minor && this.patch >= patch)
+        return true
+
+    return false
+}
