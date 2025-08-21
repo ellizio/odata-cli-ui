3fecc5a9b0c43a1d36b32760100e5db75d02e83c
ellizio <profillive@mail.ru>
2025-08-21 16:02:49 +0300

    Added scroll to Generation Arguments tab

--- src/rider/main/kotlin/ru/ellizio/odatacliui/extensions/UiExtensions.kt
+++ src/rider/main/kotlin/ru/ellizio/odatacliui/extensions/UiExtensions.kt
@@ -2,8 +2,19 @@ package ru.ellizio.odatacliui.extensions
 
 import com.intellij.ui.components.JBTextField
 import com.intellij.ui.dsl.builder.Cell
+import com.intellij.ui.dsl.builder.Panel
+import com.intellij.ui.dsl.builder.panel
+import ru.ellizio.odatacliui.ui.ScrollableDialogPanel
 
 fun Cell<JBTextField>.emptyText(text: String): Cell<JBTextField> {
     component.emptyText.text = text
     return this
+}
+
+fun scrollablePanel(init: Panel.() -> Unit): ScrollableDialogPanel {
+    val content = panel(init)
+    return ScrollableDialogPanel(content).apply {
+        border = null
+        isOverlappingScrollBar = false
+    }
 }
